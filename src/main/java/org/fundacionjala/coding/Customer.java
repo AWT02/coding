b31f8aa88a641797d63a9f9c38cbde0a941d81a5
rrch.cr8 <rrch.cr8@gmail.com>
2019-04-08 16:34:53 -0400

    more checkstyles fixing

--- src/main/java/org/fundacionjala/coding/Customer.java
+++ src/main/java/org/fundacionjala/coding/Customer.java
@@ -4,52 +4,82 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class should be called VideoStore instead of costumner
+ * This class should be called VideoStore instead of costumner.
  */
 public class Customer {
+    /**
+     *name of the costumer.
+     */
     private final String name;
+    /**
+     * list of rentals.
+     */
     private List<Rental> orderList = new ArrayList<>();
 
+    /**
+     * @param name is the namme of the costumer
+     */
     public Customer(final String name) {
         this.name = name;
     }
 
+    /**
+     * @param arg object rental type.
+     */
     public void addRental(final Rental arg) {
         this.orderList.add(arg);
     }
 
+    /**
+     * @return name
+     */
     public String getName() {
         return this.name;
     }
 
-    public double orderCost(){
-        double sum=0;
-        for (Rental rent:orderList){
-            sum+=rent.getrentCost();
+    /**
+     *
+     * @return the total cost of the order
+     */
+    public double orderCost() {
+        double sum = 0;
+        for (Rental rent:orderList) {
+            sum += rent.getrentCost();
         }
         return sum;
     }
 
-    public int frequentRenderPoints(){
-        int frequentRenderPoints=0;
-        for (Rental rent:orderList){
-            frequentRenderPoints+=1;
-            if(rent.getMovie().getPriceCode()==1 && rent.daysRented>1){
-                frequentRenderPoints+=1;
+    /**
+     *
+     * @return the frecquent point that a costumer gets
+     */
+    public int frequentRenderPoints() {
+        int frequentRenderPoints = 0;
+        for (Rental rent:orderList) {
+            frequentRenderPoints += 1;
+            if (rent.getMovie().getPriceCode() == 1
+                    &&
+                    rent.getDaysRented() > 1) {
+                frequentRenderPoints += 1;
             }
         }
         return frequentRenderPoints;
     }
 
+    /**
+     *
+     * @return an statement that will be printed
+     */
     public String statement() {
         String result = "Rental Record for " + this.getName() + "\n";
         for (final Rental rent : this.orderList) {
-            result += "\t" + rent.getMovie().getTitle() + "\t" + String.valueOf(rent.getrentCost()) + "\n";
+            result += "\t" + rent.getMovie().getTitle() + "\t"
+                    + rent.getrentCost() + "\n";
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(this.orderCost()) +
-                "\n";
-        result += "You earned " + String.valueOf(this.frequentRenderPoints())
+        result += "Amount owed is " + this.orderCost()
+                + "\n";
+        result += "You earned " + this.frequentRenderPoints()
                 +
                " frequent renter points";
 
--- src/main/java/org/fundacionjala/coding/Rental.java
+++ src/main/java/org/fundacionjala/coding/Rental.java
@@ -1,27 +1,53 @@
 package org.fundacionjala.coding;
-
 import org.fundacionjala.coding.org.fundacionjala.coding.movietypes.MovieType;
 
+/**
+ * @author carlangas.
+ */
 class Rental {
+    /**
+     * movie carlangas.
+     */
+    private Movie movie;
+    /**
+     * days that the movie will be rented.
+     */
+    private int daysRented;
 
-    public Movie movie;
-    public int daysRented;
-
-    public Rental(final Movie movie, final int daysRented) {
+    /**
+     * This is a constructor.
+     * @param movie type
+     * @param daysRented type
+     */
+    Rental(/*hola*/ final Movie movie, /*hola*/ final int daysRented) {
         this.movie = movie;
         this.daysRented = daysRented;
     }
 
+    /**
+     *
+     * @return days rented
+     */
     public int getDaysRented() {
         return this.daysRented;
     }
 
+    /**
+     *
+     * @return movie object
+     */
     public Movie getMovie() {
         return this.movie;
     }
 
+    /**
+     *
+     * @return the calculation considering days rented and base cost, also
+     * the price code
+     */
     public double getrentCost() {
-        final MovieType movieType = this.movie.createtype(this.movie.getPriceCode());
+        final MovieType movieType = this.movie.
+                createtype(this.movie.getPriceCode());
         return movieType.calculatePay(this.daysRented);
     }
 }
