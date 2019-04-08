15c1ffd154c5a963e5ce0e5e232a94049219ec6c
88mary256 <mary.jlm.2006@gmail.com>
2019-04-08 14:37:59 -0400

    Updated tests to verify movie and rent details

--- src/test/java/org/fundacionjala/movies/RentalTest.java
+++ src/test/java/org/fundacionjala/movies/RentalTest.java
@@ -11,10 +11,10 @@ public class RentalTest {
     /** this test verify if rent detail is get correctly. **/
     @Test
     public void testGetDetail() {
-        final Rental rental = new Rental(new RegularMovie("Terminator"), 3);
+        final Rental rental = new Rental(new RegularMovie("Terminator"), 2);
         assertTrue(rental.getDetail().contains(rental.getMovie().getTitle()));
-        assertEquals(3, rental.getDaysRented());
-        assertEquals("\tTerminator\t1.5\n", rental.getDetail());
+        assertEquals(2, rental.getDaysRented());
+        assertEquals("\tTerminator\t2.0\n", rental.getDetail());
         assertEquals(PriceCode.REGULAR, rental.getMovie().getPriceCode());
     }
 }
