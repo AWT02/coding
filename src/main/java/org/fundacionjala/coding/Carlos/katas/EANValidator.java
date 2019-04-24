605f3a05a699e3e4ef87d780fa176aaee8094bfa
rrch.cr8 <rrch.cr8@gmail.com>
2019-04-24 17:17:39 -0400

    check style fixes

--- src/main/java/org/fundacionjala/coding/Carlos/katas/EANValidator.java
+++ src/main/java/org/fundacionjala/coding/Carlos/katas/EANValidator.java
@@ -4,25 +4,30 @@ package org.fundacionjala.coding.Carlos.katas;
  * THIS IS KATA FROM CODEWARS CALLED EAN VALIDATOR.
  */
 class EANValidator {
-    static boolean validate(String eanCode) {
+    /**
+     * @param eanCode is the ean code.
+     * @return true or false
+     */
+    static boolean validate(final String eanCode) {
         int sumPares = 0;
         int sumImpares = 0;
         int sum = 0;
-        boolean result;
+        final int multiplier = 3;
+        final boolean result;
         for (int i = 0; i < eanCode.length() - 1; i++) {
             if (i % 2 == 0) {
                 sumPares += Integer.parseInt(String.valueOf(eanCode.charAt(i)));
             } else {
-                sumImpares += 3 * Integer.parseInt(String.valueOf(eanCode.charAt(i)));
+                sumImpares += multiplier * Integer.parseInt(String.valueOf(eanCode.charAt(i)));
             }
         }
         sum = sumImpares + sumPares;
-        String ean = String.valueOf(eanCode.charAt(12));
-        String eanown;
+        final String ean = String.valueOf(eanCode.charAt(12));
+        final String eanown;
         if (sum % 10 == 0) {
             eanown = "0";
         } else {
-            int checksum = 10 - (sum % 10);
+            final int checksum = 10 - (sum % 10);
             eanown = checksum + "";
         }
         if (ean.equals(eanown)) {
