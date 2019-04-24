0cbb90a398be94c7185936e9b01701da819c016d
rrch.cr8 <rrch.cr8@gmail.com>
2019-04-24 18:10:37 -0400

    check style fixes on Unit Tests

--- src/test/java/org/fundacionjala/coding/carlos/katas/EanValidatorTest.java
+++ src/test/java/org/fundacionjala/coding/carlos/katas/EanValidatorTest.java
@@ -5,19 +5,31 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Some Tests of the Codewars page.
+ */
 public class EanValidatorTest {
+    /**
+     * valid case.
+     */
     @Test
-    public void test_01() {
+    public void test_one() {
         assertTrue(EanValidator.validate("9783815820865"));
     }
 
+    /**
+     * Invalid Case.
+     */
     @Test
-    public void test_02() {
+    public void test_two() {
         assertFalse(EanValidator.validate("9783815820864"));
     }
 
+    /**
+     * valid case.
+     */
     @Test
-    public void test_03() {
+    public void test_three() {
         assertTrue(EanValidator.validate("9783827317100"));
     }
 }
