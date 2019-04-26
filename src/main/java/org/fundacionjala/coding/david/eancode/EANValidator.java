d6bf7ac63f5fb336f66d74ba74632144dc73aeaf
d4vidmc <d4vidangelmc@gmail.com>
2019-04-26 18:24:12 -0400

    Refactor approch with magic numbers

--- src/main/java/org/fundacionjala/coding/david/eancode/EANValidator.java
+++ src/main/java/org/fundacionjala/coding/david/eancode/EANValidator.java
@@ -4,6 +4,9 @@ package org.fundacionjala.coding.david.eancode;
  * EAN code validator.
  */
 public final class EANValidator {
+
+    private static final int TEN = 10;
+    private static final int THREE = 3;
     /**
      * EANValidator constructor.
      */
@@ -20,16 +23,16 @@ public final class EANValidator {
         String[] part = eanCode.split("");
         int aux = 0;
         int checksum = 0;
-        final int[] numbers = {3, 10};
+
         for (int i = 1; i < eanCode.length(); i++) {
             if (i % 2 == 0) {
-                aux = (Integer.parseInt(part[i - 1]) * numbers[0]) + aux;
+                aux = (Integer.parseInt(part[i - 1]) * THREE) + aux;
             } else {
                 aux = Integer.parseInt(part[i - 1]) + aux;
             }
         }
-        if (aux % numbers[1] != 0) {
-            checksum = numbers[1] - (aux % numbers[1]);
+        if (aux % TEN != 0) {
+            checksum = TEN - (aux % TEN);
         }
         return checksum == Integer.parseInt(part[eanCode.length() - 1]);
     }
