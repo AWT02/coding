bf247ac4d9f13ca877bced23c44bd58a5939273e
Rrch <rrch.cr8@gmail.com>
2019-05-06 00:18:09 -0400

    ean validator

--- src/main/java/org/fundacionjala/coding/carlos/eancode/EanValidator.java
+++ src/main/java/org/fundacionjala/coding/carlos/eancode/EanValidator.java
@@ -1,4 +1,49 @@
 package org.fundacionjala.coding.carlos.eancode;
 
-public class EanValidator {
+/**
+ * This is a Kata of Codewars webpage.
+ */
+public final class EanValidator {
+    /**
+     * This was required for sonarcloud check fix.
+     */
+    private EanValidator() {
+    }
+
+    /**
+     * @param eanCode is the ean code.
+     * @return true or false
+     */
+    static boolean validate(final String eanCode) {
+        int sumPares = 0;
+        int sumImpares = 0;
+        int sum = 0;
+        final int multiplier = 3;
+        final int ten = 10;
+        final boolean result;
+        for (int i = 0; i < eanCode.length() - 1; i++) {
+            if (i % 2 == 0) {
+                sumPares += Integer.parseInt(String.valueOf(eanCode.charAt(i)));
+            } else {
+                sumImpares += multiplier
+                        * Integer.parseInt(String.valueOf(eanCode.charAt(i)));
+            }
+        }
+        sum = sumImpares + sumPares;
+        final String ean = String.valueOf(eanCode.charAt(12));
+        final String eanown;
+        if (sum % ten == 0) {
+            eanown = "0";
+        } else {
+            final int checksum = ten - (sum % ten);
+            eanown = checksum + "";
+        }
+        if (ean.equals(eanown)) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
 }
