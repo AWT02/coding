ffc5e29c3828b03df95e62cd9571a26ec808bb35
d4vidmc <d4vidangelmc@gmail.com>
2019-05-07 00:59:15 -0400

    third part of final exam

--- src/main/java/org/fundacionjala/coding/david/crypto/CesarCrypto.java
+++ src/main/java/org/fundacionjala/coding/david/crypto/CesarCrypto.java
@@ -1,25 +1,42 @@
 package org.fundacionjala.coding.david.crypto;
 
-import java.util.HashMap;
+/**
+ * Kata for final exam.
+ */
+public final class CesarCrypto {
 
-public class CesarCrypto {
-     /**
+    private static final int NINETY = 90;
+    private static final int TWENTYSIX = 26;
+    private static final int SIXTYFIVE = 65;
+
+    /**
+     * CesarCrypto constructor.
+     */
+    private CesarCrypto() {
+    }
+
+    /**
      * Encode a given string.
      * @param text String
      * @param keyIndex int
      * @return boolean
      */
     public static String encode(final String text, final int keyIndex) {
+        if (text == null) {
+            return "";
+        }
         StringBuilder result = new StringBuilder();
-
         for (Character ch: text.toCharArray()) {
             int actualIndex = (int) ch;
-            if (actualIndex > (90-keyIndex)) {
-                actualIndex = actualIndex + keyIndex - 26;
-            }else {
+            if (actualIndex < SIXTYFIVE || actualIndex > NINETY) {
+                result.append(ch);
+                break;
+            } else if (actualIndex > (NINETY - keyIndex)) {
+                actualIndex = actualIndex + keyIndex - TWENTYSIX;
+            } else {
                 actualIndex = actualIndex + keyIndex;
             }
-            result.append((char)actualIndex);
+            result.append((char) actualIndex);
         }
         return result.toString();
     }
@@ -30,16 +47,74 @@ public class CesarCrypto {
      * @return boolean
      */
     public static String decode(final String text, final int keyIndex) {
+        if (text == null) {
+            return "";
+        }
         StringBuilder result = new StringBuilder();
 
         for (Character ch: text.toCharArray()) {
             int actualIndex = (int) ch;
-            if ((actualIndex-keyIndex) < 65) {
-                actualIndex = actualIndex - keyIndex + 26;
-            }else {
+            if (actualIndex < SIXTYFIVE || actualIndex > NINETY) {
+                result.append(ch);
+                break;
+            } else if ((actualIndex - keyIndex) < SIXTYFIVE) {
+                actualIndex = actualIndex - keyIndex + TWENTYSIX;
+            } else {
                 actualIndex = actualIndex - keyIndex;
             }
-            result.append((char)actualIndex);
+            result.append((char) actualIndex);
+        }
+        return result.toString();
+    }
+    /**
+     * Encode vigenere a given string.
+     * @param text String
+     * @param keyIndex String
+     * @return boolean
+     */
+    public static String encodeVigenere(final String text, final String keyIndex) {
+        if (text == null || keyIndex == null) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        for (Character ch: text.toCharArray()) {
+            int actualIndex = (int) ch;
+            if (actualIndex < SIXTYFIVE || actualIndex > NINETY) {
+                result.append(ch);
+            } else {
+                result.append(encode(ch.toString(), (int) keyIndex.charAt(i) - SIXTYFIVE + 1));
+                i++;
+            }
+            if (i == keyIndex.length()) {
+                i = 0;
+            }
+        }
+        return result.toString();
+    }
+    /**
+     * Decode vigenere a given string.
+     * @param text String
+     * @param keyIndex String
+     * @return boolean
+     */
+    public static String decodeVigenere(final String text, final String keyIndex) {
+        if (text == null || keyIndex == null) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        for (Character ch: text.toCharArray()) {
+            int actualIndex = (int) ch;
+            if (actualIndex < SIXTYFIVE || actualIndex > NINETY) {
+                result.append(ch);
+            } else {
+                result.append(decode(ch.toString(), (int) keyIndex.charAt(i) - SIXTYFIVE + 1));
+                i++;
+            }
+            if (i == keyIndex.length()) {
+                i = 0;
+            }
         }
         return result.toString();
     }
--- src/test/java/org/fundacionjala/coding/david/crypto/CesarCryptoTest.java
+++ src/test/java/org/fundacionjala/coding/david/crypto/CesarCryptoTest.java
@@ -1,43 +1,83 @@
-package org.fundacionjala.coding.david.eancode;
-        import org.fundacionjala.coding.david.crypto.CesarCrypto;
-        import org.junit.Test;
+package org.fundacionjala.coding.david.crypto;
 
-        import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test to validate EANCode.
  */
 public class CesarCryptoTest {
+    private static final int THREE = 3;
+    private static final int FIVE = 5;
     /**
-     * First Code "HOLA" with key 3.
+     * First encode "HOLA" with key 3.
      */
     @Test
     public void test01() {
-
-        assertEquals("KROD", CesarCrypto.encode("HOLA",3));
+        assertEquals("KROD", CesarCrypto.encode("HOLA", THREE));
     }
     /**
-     * Second Code "ZORRO" with key 5.
+     * Second encode "ZORRO" with key 5.
      */
     @Test
     public void test02() {
-
-        assertEquals("ETWWT", CesarCrypto.encode("ZORRO",5));
+        assertEquals("ETWWT", CesarCrypto.encode("ZORRO", FIVE));
     }
     /**
-     * First Code "HOLA".
+     * First decode "HOLA" with key 3.
      */
     @Test
     public void test03() {
-
-        assertEquals("HOLA", CesarCrypto.decode("KROD",3));
+        assertEquals("HOLA", CesarCrypto.decode("KROD", THREE));
     }
     /**
-     * First Code "ZORRO".
+     *  Second decode "ZORRO" with key 5.
      */
     @Test
     public void test04() {
-
-        assertEquals("ZORRO", CesarCrypto.decode("ETWWT",5));
+        assertEquals("ZORRO", CesarCrypto.decode("ETWWT", FIVE));
+    }
+    /**
+     *  Null value encode.
+     */
+    @Test
+    public void test05() {
+        assertEquals("", CesarCrypto.encode(null, FIVE));
+    }
+    /**
+     *  Empty value encode.
+     */
+    @Test
+    public void test06() {
+        assertEquals("", CesarCrypto.encode("", FIVE));
+    }
+    /**
+     *
+     *  Null value decode.
+     */
+    @Test
+    public void test07() {
+        assertEquals("", CesarCrypto.decode(null, FIVE));
+    }
+    /**
+     *  Empty value decode.
+     */
+    @Test
+    public void test08() {
+        assertEquals("", CesarCrypto.decode("", FIVE));
+    }
+    /**
+     *  Empty value decode.
+     */
+    @Test
+    public void test09() {
+        assertEquals("TXYP XMUNHCRC", CesarCrypto.encodeVigenere("HOLA JALASOFT", "LIMON"));
+    }
+    /**
+     *  Empty value decode.
+     */
+    @Test
+    public void test10() {
+        assertEquals("HOLA JALASOFT", CesarCrypto.decodeVigenere("TXYP XMUNHCRC", "LIMON"));
     }
 }
