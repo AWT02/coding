0d806c5f1316171612d7ae2100cda7b2fd904755
Rrch <rrch.cr8@gmail.com>
2019-05-07 20:09:39 -0400

    check style fixes

--- src/main/java/org/fundacionjala/coding/carlos/cifradocesar/CesarCrypto.java
+++ src/main/java/org/fundacionjala/coding/carlos/cifradocesar/CesarCrypto.java
@@ -17,21 +17,21 @@ public final class CesarCrypto {
      * @return encoded test.
      */
     public static String encode(final String word, final int codein) {
-
+        final int numberOfLettersInAlphabet = 26;
         StringBuilder enc = new StringBuilder();
-        int code = codein % 26;
+        int code = codein % numberOfLettersInAlphabet;
         String text = word.toUpperCase();
         for (int i = 0; i < text.length(); i++) {
             if (text.charAt(i) >= 'A' && text.charAt(i) <= 'Z') {
 
                 if ((text.charAt(i) + code) > 'Z') {
-                    enc.append((char) (text.charAt(i) + code - 26));
-                } else if ((text.charAt(i) + code) < 'A'){
-                    enc.append((char) (26+ text.charAt(i) + code ));
-                }else {
+                    enc.append((char) (text.charAt(i) + code - numberOfLettersInAlphabet));
+                } else if ((text.charAt(i) + code) < 'A') {
+                    enc.append((char) (numberOfLettersInAlphabet + text.charAt(i) + code));
+                } else {
                     enc.append((char) (text.charAt(i) + code));
                 }
-            } else{
+            } else {
                 enc.append(text.charAt(i));
             }
         }
@@ -45,21 +45,22 @@ public final class CesarCrypto {
      * @return decoded text.
      */
     public static String decode(final String word, final int codein) {
+        final int numberOfLettersInAlphabet = 26;
         StringBuilder enc = new StringBuilder();
-        int code = codein % 26;
-        code*=-1;
-        String text=word.toUpperCase();
+        int code = codein % numberOfLettersInAlphabet;
+        code *= -1;
+        String text = word.toUpperCase();
         for (int i = 0; i < text.length(); i++) {
             if (text.charAt(i) >= 'A' && text.charAt(i) <= 'Z') {
 
                 if ((text.charAt(i) + code) > 'Z') {
-                    enc.append((char) (text.charAt(i) + code - 26));
-                } else if ((text.charAt(i) + code) < 'A'){
-                    enc.append((char) (26+ text.charAt(i) + code ));
-                }else {
+                    enc.append((char) (text.charAt(i) + code - numberOfLettersInAlphabet));
+                } else if ((text.charAt(i) + code) < 'A') {
+                    enc.append((char) (numberOfLettersInAlphabet + text.charAt(i) + code));
+                } else {
                     enc.append((char) (text.charAt(i) + code));
                 }
-            } else{
+            } else {
                 enc.append(text.charAt(i));
             }
         }
